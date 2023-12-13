f795cdb7de47835a59fc32402f44364c66d82041
SEC <SEC@DESKTOP-CTT50RL>
2023-12-13 17:43:18 +0900

    12/13 17:42 푸드게시판 수정하기 DB 연결 완료 (서버컴으로 추가작업 필요 푸드게시판 수정하기 및 작성하기)

--- src/main/java/foodDB/FoodDAO.java
+++ src/main/java/foodDB/FoodDAO.java
@@ -9,9 +9,8 @@ import foodDB.FoodDTO;
 public class FoodDAO extends DBConnPool {
 	public FoodDTO getList(String visitcount, String image, String text, String title, String heartcount, String cate) {
 		FoodDTO vo = new FoodDTO();
-		
 		try {
-			String insertQuery = "INSERT INTO FOOD(head_num,visitcount, image, text, title,mbnum,heartcount, cate) VALUES(5,?,?,?,?,1,?,?)";
+			String insertQuery = "INSERT INTO FOOD(head_num, visitcount, image, text, title, mbnum, heartcount, cate) VALUES(2,?,?,?,?,1,?,?)";
 			
 			psmt = con.prepareStatement(insertQuery);
 			psmt.setString(1, visitcount);
@@ -46,4 +45,45 @@ public class FoodDAO extends DBConnPool {
 		return vo;
 
 	}
+	
+	public FoodDTO getEdit(String head_num, String visitcount, String image, String text, String title,String mbnum ,String heartcount, String cate) {
+		FoodDTO vo = new FoodDTO();
+		try {
+			String updateQuery = "UPDATE FOOD set head_num='1',visitcount=?, image=?, text=?, title=?,mbnum=1,heartcount=?, cate=? where head_num='1'";
+			
+			System.out.println(updateQuery);
+			psmt = con.prepareStatement(updateQuery);
+			psmt.setString(1, visitcount);
+			psmt.setString(2, image);
+			psmt.setString(3, text);
+			psmt.setString(4, title);
+			psmt.setString(5, heartcount);
+			psmt.setString(6, cate);
+			psmt.executeUpdate();
+
+			
+			System.out.println(visitcount + image + text + title + heartcount + cate);
+			
+			
+			vo.setVisitcount();
+			vo.setImage();
+			vo.setText();
+			vo.setTitle();
+			vo.setHeartcount();
+			vo.setCate();
+
+			System.out.println(vo.getVisitcount());
+			System.out.println(vo.getImage());
+			System.out.println(vo.getText());
+			System.out.println(vo.getTitle());
+			System.out.println(vo.getHeartcount());
+			System.out.println(vo.getCate());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return vo;
+
+	}
+	
 }

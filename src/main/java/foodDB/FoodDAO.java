04f455c73c5a622f9e547f13c149199a572a426c
SEC <SEC@DESKTOP-CTT50RL>
2023-12-19 13:48:32 +0900

    12/19 게시판 목록보기 다시수정(500 오류 뜰 시 프로퍼티스 -> jstl, ojdbc 있는지 확인)

--- src/main/java/foodDB/FoodDAO.java
+++ src/main/java/foodDB/FoodDAO.java
@@ -1,17 +1,17 @@
 package foodDB;
 
-import java.sql.Date;
-import java.sql.ResultSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
 
 import common.DBConnPool;
-import foodDB.FoodDTO;
 
 public class FoodDAO extends DBConnPool {
 	public FoodDTO getList(String visitcount, String image, String text, String title, String heartcount, String cate) {
 		FoodDTO vo = new FoodDTO();
 		try {
 			String insertQuery = "INSERT INTO FOOD(head_num, visitcount, image, text, title, mbnum, heartcount, cate) VALUES(2,?,?,?,?,1,?,?)";
-			
+
 			psmt = con.prepareStatement(insertQuery);
 			psmt.setString(1, visitcount);
 			psmt.setString(2, image);
@@ -23,14 +23,13 @@ public class FoodDAO extends DBConnPool {
 
 			System.out.println(insertQuery);
 			System.out.println(visitcount + image + text + title + heartcount + cate);
-			
-			
-			vo.setVisitcount();
-			vo.setImage();
-			vo.setText();
-			vo.setTitle();
-			vo.setHeartcount();
-			vo.setCate();
+
+			vo.setVisitcount(rs.getInt(visitcount));
+			vo.setImage(rs.getString(image));
+			vo.setText(rs.getString(text));
+			vo.setTitle(rs.getString(title));
+			vo.setHeartcount(rs.getInt(heartcount));
+			vo.setCate(rs.getString(cate));
 
 			System.out.println(vo.getVisitcount());
 			System.out.println(vo.getImage());
@@ -45,12 +44,13 @@ public class FoodDAO extends DBConnPool {
 		return vo;
 
 	}
-	
-	public FoodDTO getEdit(String head_num, String visitcount, String image, String text, String title,String mbnum ,String heartcount, String cate) {
+
+	public FoodDTO getEdit(String head_num, String visitcount, String image, String text, String title, String mbnum,
+			String heartcount, String cate) {
 		FoodDTO vo = new FoodDTO();
 		try {
 			String updateQuery = "UPDATE FOOD set head_num='1',visitcount=?, image=?, text=?, title=?,mbnum=1,heartcount=?, cate=? where head_num='1'";
-			
+
 			System.out.println(updateQuery);
 			psmt = con.prepareStatement(updateQuery);
 			psmt.setString(1, visitcount);
@@ -61,16 +61,14 @@ public class FoodDAO extends DBConnPool {
 			psmt.setString(6, cate);
 			psmt.executeUpdate();
 
-			
 			System.out.println(visitcount + image + text + title + heartcount + cate);
-			
-			
-			vo.setVisitcount();
-			vo.setImage();
-			vo.setText();
-			vo.setTitle();
-			vo.setHeartcount();
-			vo.setCate();
+
+			vo.setVisitcount(rs.getInt(visitcount));
+			vo.setImage(rs.getString(image));
+			vo.setText(rs.getString(text));
+			vo.setTitle(rs.getString(title));
+			vo.setHeartcount(rs.getInt(heartcount));
+			vo.setCate(rs.getString(cate));
 
 			System.out.println(vo.getVisitcount());
 			System.out.println(vo.getImage());
@@ -85,5 +83,55 @@ public class FoodDAO extends DBConnPool {
 		return vo;
 
 	}
-	
+
+	public int selectCount(Map<String, Object> map) {
+		int totalCount = 0;
+
+		String query = "SElECT * FROM Food";
+		if (map.get("searchWord") != null) {
+			query += "WHERE" + map.get("searchField") + " " + " LIKE '% " + map.get("searchWord") + "%'";
+		}
+
+		try {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(query);
+			rs.next();
+			totalCount = rs.getInt(1);
+		} catch (Exception e) {
+			System.out.println("게시물 수를 구하는 중 예외 발생");
+			e.printStackTrace();
+		}
+		return totalCount;
+	}
+
+	public List<FoodDTO> selectList(Map<String, Object> map) {
+		List<FoodDTO> bbs = new Vector<FoodDTO>();
+
+		String query = "SELECT * FROM FOOD";
+		if (map.get("searchWord") != null) {
+			query += " WHERE " + map.get("searchField") + " " + " Like '%" + map.get("searchWord") + "%' ";
+		}
+		
+
+		try {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(query);
+
+			while (rs.next()) {
+				FoodDTO dto = new FoodDTO();
+
+				dto.setTitle(rs.getString("title"));
+				dto.setFooddate(rs.getDate("fooddate"));
+				dto.setVisitcount(rs.getInt("visitcount"));
+
+				bbs.add(dto);
+			}
+		} catch (Exception e) {
+			System.out.println("게시물 조회 중 예외 발생");
+			e.printStackTrace();
+		}
+		return bbs;
+
+	}
+
 }

768a7b67b6d9b977d728fe64fc9d437bd3d9aedf
p.gieun <p.gieun@DESKTOP-KRSNSTM>
2024-01-02 16:45:02 +0900

    1/2 16:44 푸드게시판 수정, 삭제

---
+++ src/main/java/food/FoodDelete.java
@@ -0,0 +1,36 @@
+package food;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FoodDelete extends HttpServlet{
+	FoodDAO dao;
+	
+	public void init() throws ServletException{
+		dao = new FoodDAO();
+	}
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String check = request.getParameter("editordelete");
+		String headnum = request.getParameter("HEADNUM");
+		if(check.equals("삭제하기")) {
+			dao.delete(headnum);
+		} else if (check.equals("수정하기")){
+			FoodDTO dto = dao.selectView(headnum);
+			request.setAttribute("dto", dto);
+			request.getRequestDispatcher("../Main/Main.jsp?sidePage=../Food/FoodSide.jsp&contentPage=../Food/FoodEdit.jsp").forward(request, response);
+		} 
+		
+	}
+
+	@Override
+	public void destroy() {
+		dao.close();
+	}
+}
+
--- src/main/webapp/EXFFFF/Food/foodbody.jsp
+++ src/main/webapp/EXFFFF/Food/foodbody.jsp
@@ -57,7 +57,7 @@ a:hover{color: gray}
             	<table>
                 <tr>
             	<c:forEach items="${boardLists}" var="row" end = "3" varStatus="loop">
-                		<td style="width:300px;"> <a href="../Main/Main.jsp?sidePage=../Food/foodside.jsp&contentPage=../Food/FoodView.do?headnum=${ row.head_num }"> 
+                		<td style="width:300px;"> <a href="../Main/Main.jsp?sidePage=../Food/foodside.jsp&contentPage=../Food/FoodView.do?headnum=${ row.headnum }"> 
                 		<c:if test="${ row.image != null }"> <img src="../img/${ row.image }" alt="푸드게시판" /> </c:if>
                 		<c:if test="${ row.image == null }"> <img src="../img/test2.jpeg" alt = '푸드게시판'> </c:if> </a> </td>
                 </c:forEach>
